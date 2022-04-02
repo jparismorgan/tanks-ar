b63adb58ee9d0c2c184bdd637eb6572721b92e26
Paris Morgan <paris@8thwall.com>
2022-04-02 16:24:10 -0700

    joystick

--- Assets/Joystick.cs
+++ Assets/Joystick.cs
@@ -13,37 +13,37 @@ public class Joystick : MonoBehaviour {
     public Transform outerCircle;
 
 	// Update is called once per frame
-	void Update () {
-        if(Input.GetMouseButtonDown(0)){
-            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+	// void Update () {
+  //       if(Input.GetMouseButtonDown(0)){
+  //           pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
 
-            circle.transform.position = pointA * -1;
-            outerCircle.transform.position = pointA * -1;
-            circle.GetComponent<SpriteRenderer>().enabled = true;
-            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
-        }
-        if(Input.GetMouseButton(0)){
-            touchStart = true;
-            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-        }else{
-            touchStart = false;
-        }
+  //           circle.transform.position = pointA * -1;
+  //           outerCircle.transform.position = pointA * -1;
+  //           circle.GetComponent<SpriteRenderer>().enabled = true;
+  //           outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+  //       }
+  //       if(Input.GetMouseButton(0)){
+  //           touchStart = true;
+  //           pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+  //       }else{
+  //           touchStart = false;
+  //       }
         
-	}
-	private void FixedUpdate(){
-        if(touchStart){
-            Vector2 offset = pointB - pointA;
-            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
-            moveCharacter(direction * -1);
+	// }
+	// private void FixedUpdate(){
+  //       if(touchStart){
+  //           Vector2 offset = pointB - pointA;
+  //           Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+  //           moveCharacter(direction * -1);
 
-            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
-        }else{
-            circle.GetComponent<SpriteRenderer>().enabled = false;
-            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
-        }
+  //           circle.transform.position = new Vector3(pointA.x + direction.x, 0, pointA.z + direction.z) * -1;
+  //       }else{
+  //           circle.GetComponent<SpriteRenderer>().enabled = false;
+  //           outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+  //       }
 
-	}
-	void moveCharacter(Vector2 direction){
-        player.Translate(direction * speed * Time.deltaTime);
-    }
+	// }
+	// void moveCharacter(Vector2 direction){
+  //       player.Translate(direction * speed * Time.deltaTime);
+  //   }
 }
